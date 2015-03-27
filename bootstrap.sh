907a1bfd013716ebbd30dbd74b4ad97e5d72b5ba
Britt Mathis <britt.mathis@gmail.com>
2015-03-27 16:02:21 -0400

    zlib

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get -y install software-properties-common
 add-apt-repository -y ppa:hvr/ghc
 apt-get update
-apt-get -y install cabal-install-1.20 ghc-7.8.4 happy-1.19.4 alex-3.1.3
+apt-get -y install cabal-install-1.20 ghc-7.8.4 happy-1.19.4 alex-3.1.3  zlib1g-dev 
 
 echo "export PATH=$PATH:~/.cabal/bin:/opt/cabal/1.20/bin:/opt/ghc/7.8.4/bin:/opt/happy/1.19.4/bin:/opt/alex/3.1.3/bin" >> /home/vagrant/.bash_profile
 
