0ac533f11ad0785391e7b3cd42874ae82c2b4669
Britt Mathis <britt.mathis@gmail.com>
2015-03-27 14:43:38 -0400

    Using ubuntu trusty now

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get -y install software-properties-common
+add-apt-repository -y ppa:hvr/ghc
+apt-get update
+apt-get -y install cabal-install-1.20 ghc-7.8.4 happy-1.19.4 alex-3.1.3
+
+echo "export PATH=$PATH:~/.cabal/bin:/opt/cabal/1.20/bin:/opt/ghc/7.8.4/bin:/opt/happy/1.19.4/bin:/opt/alex/3.1.3/bin" >> /home/vagrant/.bash_profile
